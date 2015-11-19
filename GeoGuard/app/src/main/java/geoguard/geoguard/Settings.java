5118d9d4aa95558b01d49d9e38573aa133648474
Daniel Ingersoll <dingerso@ucsc.edu>
2015-11-19 15:22:22 -0800

    Started settings and allows username storage

--- GeoGuard/app/src/main/java/geoguard/geoguard/Settings.java
+++ GeoGuard/app/src/main/java/geoguard/geoguard/Settings.java
@@ -4,14 +4,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Settings extends ActionBarActivity {
+
+    Button btnUsernameChange;
+    Button btnPasswordChange;
+    Button btnRadiusChange;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        btnUsernameChange = (Button) findViewById(R.id.btnUsernameChange);
+        btnPasswordChange = (Button) findViewById(R.id.btnPasswordChange);
+        btnRadiusChange = (Button) findViewById(R.id.btnRadius);
+
+        btnUsernameChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeUsername(v);
+            }
+        });
+
+        btnPasswordChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePassword(v);
+            }
+        });
+
+        btnRadiusChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeRadius(v);
+            }
+        });
+    }
+
+    private void changeUsername(View v){
+
+    }
+
+    private void changePassword(View v){
+
+    }
+
+    private void changeRadius(View v){
+
     }
 
     @Override
