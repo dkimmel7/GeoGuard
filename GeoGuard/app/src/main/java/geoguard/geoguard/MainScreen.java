3adce3ffeaedbc3f69e27e504a85fd177e40f661
dkimmel7 <dkimmel7@gmail.com>
2015-11-28 17:35:06 -0800

    Added procedural homebase and added all passwords to homebase now

--- GeoGuard/app/src/main/java/geoguard/geoguard/MainScreen.java
+++ GeoGuard/app/src/main/java/geoguard/geoguard/MainScreen.java
@@ -1,8 +1,10 @@
 package geoguard.geoguard;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -34,6 +36,7 @@ public class MainScreen extends Activity implements View.OnClickListener {
     Button btnMobilePass;
     Button btnInsert;
     Button btnLocation; // @GeoLocation
+    private Tracker gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,33 @@ public class MainScreen extends Activity implements View.OnClickListener {
         btnSettings.setOnClickListener(this);
         btnInsert.setOnClickListener(this);
         btnLocation.setOnClickListener(this); // @GeoLocation
+
+        SharedPreferences settings = getSharedPreferences("settings" , MODE_PRIVATE);
+        gps = new Tracker(MainScreen.this);
+        int radius = settings.getInt("radius", 0);
+        System.out.println("RAD = " + radius);
+        String homeLat = settings.getString("latitude", "");
+        String homeLong = settings.getString("longitude", "");
+        String homeLoc = "";
+        System.out.println("lat is " + homeLat);
+        System.out.println("long is " + homeLong);
+        if( !(homeLat.equals("") || homeLong.equals(""))) {
+            homeLoc = homeLat + " " + homeLong;
+        }
+        if (gps.canGetLocation()){
+            if(!homeLoc.equals("")) {
+                if (!(showCurrLoc(homeLoc, getLocation(), radius))) {
+                    btnHomeBase.setVisibility(View.GONE);
+                }
+            }
+            else{
+                btnHomeBase.setVisibility(View.GONE);
+            }
+        }
+        else {
+            btnHomeBase.setVisibility(View.GONE);
+        }
+
 
     }
 
@@ -107,5 +137,50 @@ public class MainScreen extends Activity implements View.OnClickListener {
             default:
                 break;
         }
+    }
+
+    private double getDist(double lat1, double lon1, double lat2, double lon2) {
+        double R = 6372.8; // radius of Earth in KM
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        System.out.println("DIST = " + (R * c * 1000));
+        return R * c * 1000; // returns in meters
+    }
+    private String getLocation() {
+        String locString = null;
+        if (gps.canGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            locString = Double.toString(latitude);
+            locString += " " + Double.toString(longitude);
+            /*Toast.makeText(
+                    getApplicationContext(),
+                    "Your Location is -\nLat: " + latitude + "\nLong: "
+                            + longitude, Toast.LENGTH_SHORT).show();*/
+        } else {
+            // Display alert to turn on GPS
+            gps.showSettingsAlert();
+        }
+        return locString;
+    }
+
+    private boolean showCurrLoc(String there, String here,double radius) {
+        if( there.equals("") || here.equals("")) {
+            return false;
+        }
+        android.location.Location a = gps.getLocation();
+        double thereLat = Double.parseDouble(there.substring(0, there.indexOf(" ")).trim());
+        double  hereLat = Double.parseDouble(here.substring(0, here.indexOf(" ")).trim());
+        double thereLong = Double.parseDouble(there.substring(there.indexOf(" ")).trim());
+        double  hereLong = Double.parseDouble(here.substring (here.indexOf(" ")).trim());
+        if( getDist(thereLat,thereLong,hereLat,hereLong) >= 0 && getDist(thereLat,thereLong,hereLat,hereLong) <= radius) {
+            return true;
+        }
+        return false;
     }
 }
